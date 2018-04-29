35602c8dc1e76bb91ad89d4dcd40c3a861c97797
chiiia12 <llnsbchsep12@gmail.com>
2018-04-29 15:15:07 +0900

    using props

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, Image,View } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+class Greeting extends Component {
   render() {
     return (
-      <Text>Hello world</Text>
+      <Text>Hello {this.props.name}!</Text>
     );
+  }
+}
+export default class HelloWorldApp extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <View style={{ allignItems: 'center' }}>
+      <Image source={pic} style={{ width: 193, height: 110 }} />
+        <Greeting name='Rexaaar' />
+        <Greeting name='Jaina' />
+      </View>
+   );
   }
 }
