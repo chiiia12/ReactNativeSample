ab5787eced6fba1a3c338b6aca058a9ea6e28148
chiiia12 <llnsbchsep12@gmail.com>
2018-04-29 17:30:31 +0900

    finish tutorial

--- App.js
+++ App.js
@@ -1,34 +1,42 @@
 import React, { Component } from 'react';
-import { Text, Image, View } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View } from 'react-native';
 
-class Blink extends Component {
+
+export default class FetchSample extends Component {
   constructor(props) {
     super(props);
-    this.state = { isShowingText: true };
-
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
+    this.state = { isLoading: true }
+  }
+  componentDidMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function () {
+          console.log('inside function()')
+        });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    }, 1000);
   }
   render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
-    return(
-      <Text>{display}</Text>
-    );
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
+    return (
+      <View style={{ flex: 1, paddingTop: 20 }}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => <Text>{item.title},{item.releaseYear}</Text>}
+          keyExtractor={(item, index) => index} />
+      </View>
+    )
   }
-}
-
-export default class BlinkApp extends Component{
-render(){
-  return(
-    <View>
-        <Blink text="I love to blink"/>
-        <Blink text="Yes blinking is so great"/>
-        <Blink text="Why did they ever take this out of HTML"/>
-        <Blink text="Look at me look at me look at me"/>
-     </View>
-  )
-}
 }
